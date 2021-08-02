c30a9cfc934293395fd0d355d4f91b717de5c4fe
Dipankar Gogoi112 <dgogoi11@in.ibm.com>
2021-08-02 18:28:10 +0530

    update

--- golang-sample/main.go
+++ golang-sample/main.go
@@ -16,58 +16,55 @@
 
 package main
 import (
+	"fmt"
 	"log"
-	"github.com/go-openapi/loads"
-	"github.com/go-openapi/runtime/middleware"
-	"github.com/scraly/http-go-server/pkg/swagger/server/restapi"
-	"github.com/scraly/http-go-server/pkg/swagger/server/restapi/operations"
+	"net/http"
+	"os"
 )
-// func main() {​​​​​​​​
-// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {​​​​​​​​
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 // 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-// 	}​​​​​​​​)
+// 	})
 // 	log.Println("Listening on localhost:8080")
 // 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }​​​​​​​​
-func main() {​​​​​​​​
+// }
+func main() {
 	// Initialize Swagger
 	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
-	if err != nil {​​​​​​​​
+	if err != nil {
 		log.Fatalln(err)
-	}​​​​​​​​
+	}
 	api := operations.NewHelloAPI(swaggerSpec)
 	server := restapi.NewServer(api)
-	defer func() {​​​​​​​​
-		if err := server.Shutdown(); err != nil {​​​​​​​​
+	defer func() {
+		if err := server.Shutdown(); err != nil {
 			// error handle
 			log.Fatalln(err)
-		}​​​​​​​​
-	}​​​​​​​​()
+		}
+	}()
 	server.Port = 8080
 	// Implement the CheckHealth handler
 	// api.CheckHealthHandler = operations.CheckHealthHandlerFunc(
-	// 	func(user operations.CheckHealthParams) middleware.Responder {​​​​​​​​
+	// 	func(user operations.CheckHealthParams) middleware.Responder {
 	// 		return operations.NewCheckHealthOK().WithPayload("OK")
-	// 	}​​​​​​​​)
+	// 	})
 	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
 	// Implement the GetHelloUser handler
 	// api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(
-	// 	func(user operations.GetHelloUserParams) middleware.Responder {​​​​​​​​
+	// 	func(user operations.GetHelloUserParams) middleware.Responder {
 	// 		return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
-	// 	}​​​​​​​​)
+	// 	})
 	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
 	// Start server which listening
-	if err := server.Serve(); err != nil {​​​​​​​​
+	if err := server.Serve(); err != nil {
 		log.Fatalln(err)
-	}​​​​​​​​
-}​​​​​​​​
+	}
+}
 //Health route returns OK
-func Health(operations.CheckHealthParams) middleware.Responder {​​​​​​​​
+func Health(operations.CheckHealthParams) middleware.Responder {
 	return operations.NewCheckHealthOK().WithPayload("OK")
-}​​​​​​​​
+}
 //GetHelloUser returns Hello + your name
-func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {​​​​​​​​
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
 	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
-}​​​​​​​​
-
-
+}
